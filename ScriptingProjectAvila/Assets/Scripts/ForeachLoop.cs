fb2aafdd379c01b1a06e225988c54c6325edad44
MatthewAvila <709938@student.dusd.net>
2022-11-03 11:47:37 -0700

    Added Things
    Put ruby and made her move and also created tile maps and palett

---
+++ ScriptingProjectAvila/Assets/Scripts/ForeachLoop.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForeachLoop : MonoBehaviour
+{
+    void Start()
+    {
+        string[] strings = new string[3];
+
+        strings[0] = "First string";
+        strings[1] = "Second String";
+        strings[2] = "Third String";
+
+        foreach(string item in strings)
+        {
+            print (item);
+        }    
+    }
+}
